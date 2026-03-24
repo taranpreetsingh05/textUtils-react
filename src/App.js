ad6f68ca23c1dda1b70ec0a905ed672f2a8be27a
Taranpreet Singh <taranpreetsingh7678@gmail.com>
2026-03-24 19:46:20 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,60 @@
+import logo from './logo.svg';
+import './App.css';
+import Navbar from './components/Navbar';
+import Textform from './components/Textform';
+import About from './components/about';
+import { useState } from 'react';
+import Alert from './components/Alert'
+import { BrowserRouter as Router, Switch,Route,Link} from "react-router-dom";
+let name="Taran";
+function App() {
+  const [mode,setMode]=useState('light');//whether dark mode is eabled on it
+  const [alert,setAlert]=useState(null);
+  const showAlert=(message,type)=>{
+    setAlert({
+      msg:message,
+      type:type
+    })
+    setTimeout(()=>{
+      setAlert(null);
+    },1500)
+  }
+  const toggleMode=()=>{
+    if(mode=='light'){
+      setMode('dark');
+      document.body.style.backgroundColor='black'
+      document.body.style.color='white'
+      showAlert("dark mode has been enabled","success")
+      document.title="dark mode aa gaya"
+    }
+    else {setMode('light');
+      document.body.style.backgroundColor='white'
+      document.body.style.color='black'
+      showAlert("light mode has been enabled","success")
+
+    }
+  }
+  return (
+    <>
+    <Router>
+<Navbar title="TextUtils" text="about textutils" mode={mode} toggleMode={toggleMode}/>
+<Alert alert={alert}/>
+<div className="container my-3">
+  <Switch>
+  <Route exact path="/about">{/*always use exact as it is a good practice nd it allows u to access one componet only if it matches exactly*/ }
+    <About />
+  </Route>
+  <Route exact path="/">
+<Textform showAlert={showAlert} mode={mode}/>
+  </Route>
+</Switch>
+
+{/*<Textform heading="enter the text to analyze"/>*/}
+{/*<About/>*/}
+</div>
+</Router>
+   </>
+  );
+}
+
+export default App;
---
+++ src/components/Textform.js
@@ -0,0 +1,62 @@
+import React, {useState} from 'react'
+export default function Textform(props) {
+    const handlespace=()=>{
+        let newText=text.split(/[ ]+/);
+        setText(newText.join(" "));
+        props.showAlert("spaces handled","success");
+
+    }
+    const handleupperclick=()=>{
+        console.log("Upper case was clicked"+ text);
+        let newtext=text.toUpperCase();
+        setText(newtext);
+        props.showAlert("converted to upper case","success");
+    }
+        const handleonChange=(event)=>{
+        console.log("there is a change");
+        setText(event.target.value);
+    }
+    const handleloclick=()=>{
+        console.log("Upper case was clicked"+ text);
+        let newtext=text.toLowerCase();
+        setText(newtext);
+        props.showAlert("converted to lower case","success");
+
+    }
+   { /*const handlecolorclick=()=>{
+        let color1="white";
+        setColor(color1);
+    }
+    const handlerecolorclick=()=>{
+        let color1="black";
+        setColor(color1);
+    }*/}
+    const[text, setText]=useState('');
+    const[color,setColor]=useState("black")
+  return (
+    <>
+    <div className='container'>
+<h1>{props.heading}</h1>
+<div className="mb-3">
+<textarea style={{ backgroundColor: props.mode === 'light' ? 'white' : '#212529',color:props.mode === 'light' ? 'black' : 'white' }}   className="form-control" value={text} onChange={handleonChange}  id="mybox" rows="3"></textarea>
+</div>
+<button className="btn btn-primary mx-2" onClick={handleupperclick}>Convert to Upper case</button>
+<button className="btn btn-primary mx-2" onClick={handleloclick}>Convert to Lower case</button>
+{/*<button className="btn btn-primary mx-2" onClick={handlecolorclick}>changes color</button>
+<button className="btn btn-primary mx-2" onClick={handlerecolorclick}>back to normal color</button>*/}
+<button className="btn btn-primary mx-2" onClick={handlespace}>handle spaces</button>
+
+
+
+
+</div>
+<div className="container my-2">
+  <h1>your text summary</h1>
+  <p>{text.split(" ").length}  words {text.length} chars</p>
+  <p>time taken to read = {0.008*text.split(" ").length} mins</p>
+  <h2>preview</h2>
+  <p style={{ color: props.mode === 'light' ? 'black' : 'white' }}>{text.length>0?text:"enter something in textbox above to preview"}</p>
+</div>
+</>
+  )
+}
