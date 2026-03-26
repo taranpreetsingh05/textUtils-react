3c8f1a72f2d0eba9cdf5e6dc3a6400e15f0b008c
Taranpreet Singh <taranpreetsingh7678@gmail.com>
2026-03-26 13:00:42 +0530

    added color pallets

--- src/App.js
+++ src/App.js
@@ -19,19 +19,29 @@ function App() {
       setAlert(null);
     },1500)
   }
-  const toggleMode=()=>{
-    if(mode=='light'){
+  const removebodyclasses=()=>{
+    document.body.classList.remove('bg-light')
+    document.body.classList.remove('bg-dark')
+    document.body.classList.remove('bg-warning')
+    document.body.classList.remove('bg-danger')
+    document.body.classList.remove('bg-success')
+
+  }
+  const toggleMode=(cls)=>{
+    removebodyclasses()
+    document.body.classList.add('bg-'+cls)
+    if(mode==='light'){
       setMode('dark');
       document.body.style.backgroundColor='black'
       document.body.style.color='white'
       showAlert("dark mode has been enabled","success")
-      document.title="dark mode aa gaya"
+      // document.title="dark mode"
     }
     else {setMode('light');
       document.body.style.backgroundColor='white'
       document.body.style.color='black'
       showAlert("light mode has been enabled","success")
-
+      // document.title="light mode"
     }
   }
   return (
